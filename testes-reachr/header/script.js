94d45315991c2ce164702a31094a4da306a27052
paulogabriel <paulogabrieldeoliveira2@gmail.com>
2024-05-03 21:22:23 -0300

    projetando tela

---
+++ testes-reachr/header/script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var menuOptions = document.getElementsByClassName('options');
+    var menu = document.getElementsByClassName('pages');
+    var menuOpen = document.getElementsByClassName('open');
+
+
+    menuOpen[0].addEventListener('click', function() {
+        menuOptions[0].classList.toggle('exp');
+    });
+    console.log(document.body)
+;};
